f0245e0368fddab37367524c432c8258794475c7
iammoy <iammoy23@gmail.com>
2024-02-22 15:23:05 -0500

    shifting preprocessing to utils

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -7,6 +7,7 @@ from line_profiler import LineProfiler
 import functools
 import time
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 def getKeys(file) ->list:
     """
@@ -24,8 +25,10 @@ def getKeys(file) ->list:
 
 def getDFOptimized(folder_path, keys_data):
     data = []
-    for file in os.listdir(folder_path):
-        if file.endswith(".xml"):
+    file_list = [file for file in os.listdir(folder_path) if file.endswith(".xml")]
+    print("Total XML Files:", len(file_list))
+    with tqdm(total=len(file_list)) as pbar:
+        for file in file_list:
             with open(os.path.join(folder_path, file), encoding='utf-8') as f:
                 xml_data = f.read()
                 try:
@@ -36,16 +39,56 @@ def getDFOptimized(folder_path, keys_data):
                     print("Error with File Name:", file)
                     print(e)
                     continue
+            pbar.update(1)
     df = pd.DataFrame(data, columns=keys_data)
     return df
 
+def str_to_dict(string):
+    if isinstance(string, str):
+        dict_str = string.replace("OrderedDict([", "").replace("])", "")
+        return ast.literal_eval(dict_str)
+    else:
+        return None
+
+def preprocess(df) -> pd.DataFrame:
+    """
+    args: data: pd.DataFrame
+    returns: pd.DataFrame
+
+    Preprocess the data
+    """
+    df['Institution'] = df['Institution'].apply(str_to_dict)
+    df['AwardInstrument'] = df['AwardInstrument'].apply(str_to_dict)
+    df['Organization'] = df['Organization'].apply(str_to_dict)
+    df['Organization'] = df['Organization'].apply(str_to_dict)
+    df['Investigator'] = df['Investigator'].apply(str_to_dict)
+    df['Performance_Institution'] = df['Performance_Institution'].apply(str_to_dict)
+    df['ProgramElement'] = df['ProgramElement'].apply(str_to_dict)
+    df['ProgramOfficer'] = df['ProgramOfficer'].apply(str_to_dict)
+
+    df['Appropriation'] = df['Appropriation'].apply(lambda x: str_to_dict(x) if x is not None else None)
+    df['Fund'] = df['Fund'].apply(lambda x: str_to_dict(x) if x is not None else None)
+    df['ProgramReference'] = df['ProgramReference'].apply(lambda x: str_to_dict(x) if x is not None else None)
+    df['AwardInstrument'] = df['AwardInstrument'].apply(lambda x: x['Value'] if x is not None else None)
+    df['AwardEffectiveDate'] = pd.to_datetime(df['AwardEffectiveDate'])
+    df['AwardExpirationDate'] = pd.to_datetime(df['AwardExpirationDate'])
+    df['MinAmdLetterDate'] = pd.to_datetime(df['MinAmdLetterDate'])
+    df['MaxAmdLetterDate'] = pd.to_datetime(df['MaxAmdLetterDate'])
+    return df
 
 def main() -> None:
-    sample_file = r'/home/notorious/Documents/VirtuosoResearch/generating-novel-contents/Test/1351716.xml'
-    folder_path = r'/home/notorious/Documents/VirtuosoResearch/generating-novel-contents/Test/'
+    sample_file = r'/media/notorious/AdityaExtDI/data/1351716.xml'
+    folder_path = r'/media/notorious/AdityaExtDI/data/'
     keys_data = getKeys(sample_file)
-    df_opimized = getDFOptimized(folder_path, keys_data)
-    print(df_opimized)
+    print("################## Converting XML to CSV ##############################")
+    df = getDFOptimized(folder_path, keys_data)
+    print("################# CSV File Created ##############################")
+    df = preprocess(df)
+    print("################ preprocessing done ##############################")
+    df.to_csv('data.csv')
+    print("################ CSV File Saved ##############################")
+   
+
 
 if __name__ == '__main__':
     main()
